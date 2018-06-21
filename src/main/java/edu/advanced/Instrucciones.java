650645c758a274fbda15c26bb4bfb3cc1e15d18e
Juan J. Garcia <j2gl.edu@gmail.com>
2018-06-21 11:15:18 -0600

    [solution] Add solution

--- src/main/java/edu/advanced/Instrucciones.java
+++ src/main/java/edu/advanced/Instrucciones.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class Instrucciones {
 
     public static void mostrarArreglo(int[] arreglo) {
-      for (int i=0; i <= arreglo.length; i++){
+      for (int i=0; i < arreglo.length; i++){
         System.out.println("Pos " + i + ": " + arreglo[i]);
       }
     }
   
-    public static String obtenerDiaComoHoy(int anio){
+    public static String obtenerEvento(int anio) {
       String s = "";
       switch (anio){
         case 1524: 
@@ -29,12 +29,17 @@ public class Instrucciones {
       return s;
     }
   
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        int val = reader.nextInt();
-        String dia = Instrucciones.obtenerDiaComoHoy(val);
+
+        System.out.print("Ingrese año: ");
+        int anio = reader.nextInt();
+        String mensaje = Instrucciones.obtenerEvento(anio);
+        System.out.println(mensaje);
+
         int[] numeros = {1,3,5,7,9};
         Instrucciones.mostrarArreglo(numeros);
+        reader.close();
     }
 
   }
